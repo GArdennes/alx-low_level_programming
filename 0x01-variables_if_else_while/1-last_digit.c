a3f9dd03cd3954d0a528783c1000a097752f89d4
GArdennes <thekevin.afachao@gmail.com>
2023-02-17 05:15:07 -0800

    Executable to determine last digit of integer value

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	if (ls < 6 && ls != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ls);
 	else if (ls > 5)
-		printf("Last digit of %d is %d and is less than 5\n", n, ls);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ls);
 	else if (ls == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ls);
 	return (0);
