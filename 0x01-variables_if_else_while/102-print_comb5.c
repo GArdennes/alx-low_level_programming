79b52e03384578faaa31eb27b8061f4aefb12ff6
GArdennes <thekevin.afachao@gmail.com>
2023-02-18 02:57:33 -0800

    Executable that prints combination of numbers from 0 - 9

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Executable using putchar function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int a;
+
+	for (i = 0; i < 100; ++i)
+	{
+		for (a = i + 1; a < 100; ++a)
+		{
+			putchar('0' + i/10);
+			putchar('0' + i%10);
+			putchar(' ');
+			putchar('0' + a/10);
+			putchar('0' + a%10);
+			if (i == 99 && a == 99)
+			{
+				putchar('\n');
+			} else {
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
