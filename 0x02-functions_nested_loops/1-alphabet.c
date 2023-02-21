cfaf7808f8fea38fa3e935448774e6985aec4693
GArdennes <thekevin.afachao@gmail.com>
2023-02-21 05:11:16 -0800

    Executable that prints the lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Executable printing lowercase alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	print_alphabet_x10();
+	putchar('\n');
+	return (0);
+}
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+}
