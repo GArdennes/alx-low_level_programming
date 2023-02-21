482e8f1395ff26cee608f165a1f70e4b4ff00d0b
GArdennes <thekevin.afachao@gmail.com>
2023-02-21 09:35:00 -0800

    Executable for sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - Function for sum of two integers
+ * @n: The character to sum
+ * @x: The character to sum
+ *
+ * Return: On success 1
+ * On error: 0 is returned
+ */
+
+int add(int n, int x)
+{
+	int sum;
+
+	sum = n + x;
+
+	_putchar('0' + sum);
+
+	return (sum);
+}
