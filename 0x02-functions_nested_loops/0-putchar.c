edfad4fa7fbf6dfd33fc8ed60f11316b6643d9cc
GArdennes <thekevin.afachao@gmail.com>
2023-02-21 04:51:09 -0800

    Executable printing _putchar without printf or puts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Executable printing putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n[] = "_putchar";
+	int i;
+
+	for (i = 0; i < (sizeof(c) - 1); i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+}
