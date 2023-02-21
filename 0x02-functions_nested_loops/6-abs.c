0f8f2e24918670d92b77ae4e914d6151d27f164d
GArdennes <thekevin.afachao@gmail.com>
2023-02-21 08:08:32 -0800

    Executable to print absolute value of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function returning absolute value of variable
+ * @n: The characteer to print
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
+ */
+
+int _abs(int n)
+{
+	_putchar(abs(n));
+	return (1);
+}
+
