f1d027f4d204b3ef5b4c966677fc8bedc41d3938
GArdennes <thekevin.afachao@gmail.com>
2023-02-22 00:05:32 -0800

    Executable for times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,27 @@
 
 void times_table(void)
 {
-	int i;
-	int a;
-	int product;
+	int i, a, product, d1, d2;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (a = 0; a <= 9; a++)
 		{
 			product = i * a;
-			printf("%d", product);
-			if (a == 9)
-				printf("\n");
+			d1 = product / 10;
+			if (d1 != 0)
+				_putchar('0' + d1);
 			else
-				printf(", ");
+				_putchar(' ');
+			d2 = product % 10;
+			_putchar('0' + d2);
+			if (a == 9)
+				_putchar('\n');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 }
