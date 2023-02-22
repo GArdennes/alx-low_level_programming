a372ac059a68f9adb0e90a833289e4998410e46a
GArdennes <thekevin.afachao@gmail.com>
2023-02-22 11:36:08 -0800

    Executable for a Fibonacci sequence starting from 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all the first 50 Fibonacci sequence starting from 1 and 2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c = 0;
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+
+	for (i = 0; i < 49; i++)
+	{
+		c = a + b;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		
+		if (i == 48)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
