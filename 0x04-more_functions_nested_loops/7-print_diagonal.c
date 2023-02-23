f4488038afc2b57d4786d60d2312e545c8f15a74
GArdennes <thekevin.afachao@gmail.com>
2023-02-23 06:34:04 -0800

    Corrections with Betty

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,22 @@
 
 void print_diagonal(int n)
 {
-	int i, a;
+	int i = 0, a = 0;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		while (i < n)
 		{
-			for (a = 0; a < i; a++)
+			while (a < i)
+			{
 				_putchar(' ');
+				a++;
+			}
+			i++;
 			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
