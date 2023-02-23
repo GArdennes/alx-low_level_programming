b607da541a7f0f87e5fc200bac1d23593fad0e4b
GArdennes <thekevin.afachao@gmail.com>
2023-02-23 04:58:46 -0800

    Function that prints escape character n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints an escape character n times
+ * @n - Character to check
+ */
+
+void print_diagonal(int n)
+{
+	int i, a;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (a = 0; a <= i; a++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
