03002c3ebf89a19968d176ba02702313fad7030c
GArdennes <thekevin.afachao@gmail.com>
2023-02-23 04:48:10 -0800

    Function that displays numbers from 0 to 14 10x times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that displays numbers from 0 to 14 10 times
+ */
+
+void print_most_numbers(void)
+{
+	int i, a;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+				_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+		}
+		_putchar('\n');
+	}
+}
