180f727731f7d3461c11a2639dfd543c78f3e8bb
GArdennes <thekevin.afachao@gmail.com>
2023-02-23 05:53:43 -0800

    Corrections with Betty

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,14 +2,15 @@
 
 /**
  * mul - function that multiplies two integers
- * @a - Character to check
- * @b - Character to check
+ * @a: Character to check
+ * @b: Character to check
  * Return: 0
  */
 
 int mul(int a, int b)
 {
 	int prod;
+
 	prod = a * b;
 	return (prod);
 }
