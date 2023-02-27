ac2ef59022d1379aabe3af0bbfd22e5fad9835e6
GArdennes <thekevin.afachao@gmail.com>
2023-02-27 11:59:13 -0800

    function that copies one string into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies one string into another
+ * @dest: Character to check
+ * @src: Character to check
+ * Return: String
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int = a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';	
+	return (dest);
+}
