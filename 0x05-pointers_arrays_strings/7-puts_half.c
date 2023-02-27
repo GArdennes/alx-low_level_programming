52d56b9ec44b244f39630f7f946ca73ca6a97baa
GArdennes <thekevin.afachao@gmail.com>
2023-02-27 11:37:23 -0800

    Function that prints other half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half a string
+ * @str: Character to check
+ */
+
+void puts_half(char *str)
+{
+	unsigned long int i;
+	int ehalf;
+	int ohalf;
+
+	ehalf = strlen(str) / 2;
+	ohalf = (strlen(str) - 1) / 2;
+
+	if (strlen(str) % 2 == 0)
+	{
+		for (i = ehalf; i < strlen(str); i++)
+			_putchar(str[i]);
+	} else
+	{
+		for (i = ohalf; i < strlen(str); i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
