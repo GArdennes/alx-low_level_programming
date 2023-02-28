ac3a47f7dc92d90ddbb9cd53471a0d2424123a43
GArdennes <thekevin.afachao@gmail.com>
2023-02-28 02:41:00 -0800

    Function to print integer element in string

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - function that prints integers from string
+ * @s: Character to check
+ *
+ * Return: integer success
+ */
+
+int _atoi(char *s)
+{
+	unsigned long int i;
+	int j, check;
+	int sign = 1;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == 45)
+			sign *= -1;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			check = 1;
+
+			j = (j * 10) + (s[i] - '0');
+			i++;
+		}
+		if (check  == 1)
+			break;
+	}
+	j *= sign;
+
+	return (j);
+}
