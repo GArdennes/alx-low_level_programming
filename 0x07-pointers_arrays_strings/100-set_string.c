e26b0171a42506e09d593efb9d1c331448d6a416
GArdennes <thekevin.afachao@gmail.com>
2023-03-07 01:07:54 -0800

    A function for a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - A function to point to a character
+ * @s: Character to check
+ * @to: Character to check
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
