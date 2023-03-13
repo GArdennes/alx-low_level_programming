1b0c0724d483faebbd4866701478f15792822d79
GArdennes <thekevin.afachao@gmail.com>
2023-03-13 02:38:22 -0700

    Program that prints the results of multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the executable file
+ * @argc: Character to check
+ * @argv: Character to print
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (i * j));
+		return (0);
+	}
+}
