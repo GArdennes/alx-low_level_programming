0c111ec0e6a7dd4c466645a1ba1dd052cad2b608
GArdennes <thekevin.afachao@gmail.com>
2023-03-14 06:10:18 -0700

    Function that creates array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates a 2D array
+ * @width: The number of columns to check
+ * @height: The number of rows to check
+ *
+ * Return: On success result.
+ * On error, NULL is returned, and errno is set appropriately.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
