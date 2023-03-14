a521a99d751ca5a17e7c7fdb093bdb8b77fe8420
GArdennes <thekevin.afachao@gmail.com>
2023-03-14 05:42:45 -0700

    Function to create a copy of string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - creates a copy of string
+ * @str: The string to check
+ *
+ * Return: On success result.
+ * On error, NULL is returned, and errno is set appropriately.
+ */
+
+char *_strdup(char *str)
+{
+	char *arr = malloc((strlen(str) + 1) * sizeof(char));
+	unsigned int i;
+	char temp;
+
+	if (str == NULL || arr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		temp = str[i];
+		arr[i] = temp;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
