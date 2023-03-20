b334247abb2879ec5b1789c6485001f525b20d88
GArdennes <thekevin.afachao@gmail.com>
2023-03-20 06:57:19 -0700

    A new type struct dor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - structure for dog
+ * @name: String to check
+ * @age: Number to check
+ * @owner: String to check
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
