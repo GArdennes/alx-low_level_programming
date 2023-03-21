7b964e364b4658801b8b010f0ae0da5b4ed56022
GArdennes <thekevin.afachao@gmail.com>
2023-03-21 01:01:53 -0700

    A function dog_t *new_dog(char *name, float age, char *owner)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,44 @@
 #include "dog.h"
 
 /**
+ * _strlen - prints the length of string
+ * @s: string to check
+ * Return: On success result
+ */
+int _strlen(char *s)
+{
+	int num;
+
+	num = 0;
+	while (*s != 0)
+	{
+		num++;
+		s++;
+	}
+	return (num);
+}
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @src: string source
+ * @dest: string destination
+ * Return: On success result
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *src != 0; dest++, src++, i++)
+	{
+		*dest = *src;
+	}
+	for (*dest = 0; i > 0; dest--, src--, i--)
+	{
+	}
+	return (dest);
+}
+
+/**
  * new_dog - prints a struct dog
  * @name: String to check
  * @age: Number to check
@@ -18,16 +56,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newdog == NULL)
 		return (NULL);
 
-	newdog->name = malloc(strlen(name) + 1);
-	newdog->owner = malloc(strlen(owner) + 1);
+	newdog->name = malloc(_strlen(name) + 1);
+	newdog->owner = malloc(_strlen(owner) + 1);
 
 	if (newdog->name == NULL || newdog->owner == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
-	strcpy(newdog->name, name);
-	strcpy(newdog->owner, owner);
+	_strcpy(newdog->name, name);
+	_strcpy(newdog->owner, owner);
 
 	newdog->age = age;
 
