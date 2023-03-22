deef405c03a02dbf71033ac24abb01da1d956dd3
GArdennes <thekevin.afachao@gmail.com>
2023-03-22 04:29:11 -0700

    Function to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: String to check
+ * @f: function pointer to check
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Exercies for C - Function pointers
