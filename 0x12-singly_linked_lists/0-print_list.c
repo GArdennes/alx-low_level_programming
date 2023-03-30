266991fe5601eaaeed555c51e48109f38f21c65a
GArdennes <thekevin.afachao@gmail.com>
2023-03-30 10:13:30 -0700

    Function that prints elements of linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of linked list
+ * @h: Struct type to check
+ * Return: On success number of node elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (count);
+	}
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%s\n", h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
