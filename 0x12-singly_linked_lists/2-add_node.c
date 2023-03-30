557f04ed56e238fd6a81e62dd42cfc6a7bef97fc
GArdennes <thekevin.afachao@gmail.com>
2023-03-30 10:35:10 -0700

    Function to add new node as head of linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,6 +8,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -19,9 +23,9 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
