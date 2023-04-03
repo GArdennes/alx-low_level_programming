ea127ff01bd4bba23f279ada6b77a09d32c964bc
GArdennes <thekevin.afachao@gmail.com>
2023-04-03 11:48:09 -0700

    Function to add new node as head

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new node as head node
+ * @head: First node to check
+ * @n: Next node to check
+ * Return: On success address of head node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *crnt_node;
+
+	crnt_node = malloc(sizeof(listint_t));
+	if (crnt_node == NULL)
+		return (NULL);
+	crnt_node->n = n;
+	crnt_node->next = *head;
+
+	*head = crnt_node;
+
+	return (crnt_node);
+}
+
