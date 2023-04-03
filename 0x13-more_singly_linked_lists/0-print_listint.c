a96ca714880999eb94e08ca5caf2a6a3f3f50348
GArdennes <thekevin.afachao@gmail.com>
2023-04-03 11:27:26 -0700

    Functions to print nodes in linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the data in nodes of list
+ * @h: Node to check
+ * Return: On success number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *current_node = h;
+
+	while (current_node != NULL)
+	{
+		printf("%d", current_node->n);
+		current_node = current_node->next;
+		i++;
+	}
+	printf("\n");
+	return (i);
+}
