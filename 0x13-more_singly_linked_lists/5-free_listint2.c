24fc8401b86c66d73d42ce3bd544936c9ddc2127
GArdennes <thekevin.afachao@gmail.com>
2023-04-04 13:40:34 -0700

    FUnction to free memory in linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - free nodes of memory
+ *@head: header node to check
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *crnt_node, *nxt_node;
+
+crnt_node = *head;
+while (crnt_node != NULL)
+{
+	nxt_node = crnt_node->next;
+	free(crnt_node);
+	crnt_node = nxt_node;
+}
+head = NULL;
+}
