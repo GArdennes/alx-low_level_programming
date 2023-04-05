1614718f856a5b638f82f4e5d0865e6a47eebccd
GArdennes <thekevin.afachao@gmail.com>
2023-04-05 10:15:39 -0700

    Correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 
 	while (current_node != NULL)
 	{
-		printf("%d", current_node->n);
+		printf("%d\n", current_node->n);
 		current_node = current_node->next;
 		i++;
 	}
