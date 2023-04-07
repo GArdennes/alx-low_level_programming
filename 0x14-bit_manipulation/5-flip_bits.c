cff1b642d369c0f3ea12c47bf4809c7b747c4766
GArdennes <thekevin.afachao@gmail.com>
2023-04-07 12:43:58 -0700

    Function to count number of bits to flip to convert from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - determine number of bits to flip
+ * @n: Number to check
+ * @m: Number to check
+ * Return: On success result
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flip_count = 0;
+	unsigned long int x_result = n ^ m;
+
+	while (x_result != 0)
+	{
+		if ((x_result & 1) == 1)
+			flip_count++;
+		x_result = x_result >> 1;
+	}
+	return (flip_count);
+}
