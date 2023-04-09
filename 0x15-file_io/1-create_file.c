45112caf8944f56eae1aea0024b6dd45d2174ba0
GArdennes <thekevin.afachao@gmail.com>
2023-04-09 09:18:33 -0700

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file_ptr == -1)
 		return (-1);
