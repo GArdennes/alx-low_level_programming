ea8d050489d3627cf6b7d62f6bedc1c6373a01e2
GArdennes <thekevin.afachao@gmail.com>
2023-04-09 08:06:01 -0700

    Function to create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strlen - finds length of string
+ * @c: String to check
+ * Return: On success result
+ */
+size_t _strlen(char *c)
+{
+	size_t len = 0;
+
+	while (*c++ != '\0')
+	{
+		len++;
+		c++;
+	}
+	return (len);
+}
+
+/**
+ * create_file - creates new file in current directory
+ * @filename: String to check
+ * @text_content: String to check
+ * Return: On success 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_ptr;
+	size_t length;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_ptr = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+
+	if (file_ptr == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = _strlen(text_content);
+		bytes_written = write(file_ptr, text_content, length);
+
+		if (bytes_written != (ssize_t)length)
+		{
+			close(file_ptr);
+			return (-1);
+		}
+	}
+	close(file_ptr);
+	return (1);
+}
