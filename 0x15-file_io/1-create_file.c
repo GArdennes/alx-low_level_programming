99bbb994b5515bfbd69d4930be3deb18ac7823df
GArdennes <thekevin.afachao@gmail.com>
2023-04-09 09:22:25 -0700

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - finds length of string
- * @c: String to check
- * Return: On success result
- */
-size_t _strlen(char *c)
-{
-	size_t len = 0;
-
-	while (*c++ != '\0')
-	{
-		len++;
-		c++;
-	}
-	return (len);
-}
-
-/**
  * create_file - creates new file in current directory
  * @filename: String to check
  * @text_content: String to check
@@ -32,19 +15,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
 	file_ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file_ptr == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		length = _strlen(text_content);
-		bytes_written = write(file_ptr, text_content, length);
-	}
+	bytes_written = write(file_ptr, text_content, length);
 	if (bytes_written == -1)
 	{
-		close(file_ptr);
 		return (-1);
 	}
 	close(file_ptr);
