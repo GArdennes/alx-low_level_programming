154e929703f4a5b6dfdcd2903ebe4a738613fab7
GArdennes <kevin.afachao@gmail.com>
2023-08-03 12:03:34 -0700

    Function that determines endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,16 +7,10 @@
 
 int get_endianness(void)
 {
-	union
-	{
-		unsigned int i;
-		char c[4];
-	} endian_test;
+	unsigned int test = 1;
+	char *endian = (char *)&test;
 
-	endian_test.i = 1;
-
-	if (endian_test.c[0] == 1)
+	if (*endian)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
