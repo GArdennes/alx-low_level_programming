3784ad9fb08b81010a41d6888b5101450e5ff93c
GArdennes <kevin.afachao@gmail.com>
2023-08-03 11:40:46 -0700

    Function to set particular bit at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - Function to set bit at index
+ * @n: Number to check
+ * @index: Number to check
+ * Return: On success 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int buffer = 1 << index;
+
+	if (n == NULL || index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n = *n | buffer;
+
+	return (1);
+}
