853f1aacf3941de06abc40ad8dc78ec9f070d0a9
GArdennes <kevin.afachao@gmail.com>
2023-08-17 11:45:49 -0700

    Task 6 Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all the data of a list
+ * @head: dlistint_t head
+ * Return: On success int
+ */
+int sum_dlistint(dlistint *head)
+{
+	int count = 0;
+
+	if (head == NULL)
+		return (count);
+	while (head != NULL)
+	{
+		count = head->n + count;
+		head = head->next;
+	}
+	return (count);
+}
