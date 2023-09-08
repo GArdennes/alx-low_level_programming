fd897b2379853d285a91a4a167badf1cbc4ef497
GArdennes <kevin.afachao@gmail.com>
2023-09-08 10:25:14 -0700

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,21 +1,32 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - function that retrieves a value associated with a key
- * @ht: hash table
- * @key: key from hash_table
- * Return: On success char
- */
+* hash_table_get - function
+* @ht: table to search
+* @key: key to retrieve
+*
+* Description: function to get value from a passed key.
+* Return: Value of key or NULL if fail
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
-	hash_node_t *node = ht->array[index];
+	hash_node_t *tempNode;
+	unsigned long int index;
 
-	while (node != NULL)
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL || (strcmp(key, "") == 0))
+		return (NULL);
+	index = (hash_djb2((const unsigned char *)key) % (ht->size));
+	tempNode = ht->array[index];
+
+	while (tempNode)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
+		if (strcmp(key, tempNode->key) == 0)
+		{
+			return (tempNode->value);
+		}
+		tempNode = tempNode->next;
 	}
 	return (NULL);
 }
