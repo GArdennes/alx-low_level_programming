d74f786751ee5752a36584c4312b565b99c44a15
GArdennes <kevin.afachao@gmail.com>
2023-12-11 04:00:55 -0800

    Task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value using binary search algo.
+ * @array: input array
+ * @size: number of elements
+ * @value: value to search for
+ * Return: On success index else -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left = 0, right = size - 1, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+			printf("%d%s", array[i], i == right ? "\n" : ",");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			right = mid - 1;
+		else if (array[mid] < value)
+			left = mid + 1;
+	}
+	return (-1);
+}
+
